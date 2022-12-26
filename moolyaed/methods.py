0feae54be7390d8c0fa4b38426288561692de26f
AdityaVadlamani77 <aditya.vvkv.21@gmail.com>
2022-12-26 12:40:18 +0530

    python practice.

---
+++ moolyaed/methods.py
@@ -0,0 +1,23 @@
+"""x, y = 5, 10
+print(x + y)
+a, b = 15, 10
+print(a + b)"""
+
+
+def add(x, y):
+    print(x + y)
+
+
+add(50, 75)
+
+# pass a string and inside method print the characters of the string
+
+string1 = "heaven"
+string2 = "screen"
+def char(a):
+    for i in a:
+        print(i, end=", ")
+char(string1)
+print("\n")
+char(string2)
+
